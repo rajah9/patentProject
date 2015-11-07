853c9159888bc03fc2f7d0389b0e1643c92206e6
Rajah Chacko <rajah@alumni.duke.edu>
2015-11-06 22:56:23 -0500

    Version 1.3.1. Added several companies besides 3M.
    Changed CompanyFilter. Added Siemens, General Electric, Johnson&Johnson, and Dow.
    
    Added a new hive.txt. It contains hive commands that help me search for the proper stems to use, such as JOHNSON&J

--- src/main/java/mbad7090/model/CompanyFilter.java
+++ src/main/java/mbad7090/model/CompanyFilter.java
@@ -7,11 +7,16 @@ import org.apache.commons.lang.StringUtils;
  */
 public class CompanyFilter {
     // Following stems must be all uppercase.
-    static final private String _OURSTEM1 = "3M INNOVATIVE"; // allows for 3M Innovative as a stem
-    static final private String _OURSTEM2 = "3M INNOVATION"; // allows for 3M Innovation
-    static final private String _OURSTEM3 = "3M INNOVATVE";  // an unfortunate misspelling that exists in the data.
+    // settled on this regexp for all the companies to retrieve:
+    // '3M |Siemens|SIEMENS|General Electric|GENERAL ELECTRIC|Johnson\&|JOHNSON\&|^Dow |^DOW '
 
-    static private String[] matchingCompanies = { _OURSTEM1, _OURSTEM2 , _OURSTEM3};
+    static final private String _OURSTEM = "3M "; // allows for 3M Innovative or 3M Innovation as a stem
+    static final private String _SIEMENS_STEM = "SIEMENS";
+    static final private String _GE_STEM = "GENERAL ELECTRIC";
+    static final private String _J_N_J_STEM = "JOHNSON&JOHNSON";
+    static final private String _DOW_STEM = "DOW ";
+
+    static private String[] matchingCompanies = { _OURSTEM, _SIEMENS_STEM, _GE_STEM, _J_N_J_STEM, _DOW_STEM};
     /**
      * Determine whether this company is one of the target companies.
      * @param companyToTest     String of company to test, like "HONEYWELL INTERNATIONAL INC."
